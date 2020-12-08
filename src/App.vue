a41e267fd3b7cd2ea1adc1a7eb46d18e5889c233
riteshk615 <riteshk615@gmail.com>
2020-12-08 17:54:31 +0530

    first api call

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    
+    <employeelist/>
+  </div>
+</template>
+
+<script>
+
+import employeelist from './components/employeelist.vue'
+
+export default {
+  name: 'App',
+  components: {
+    
+    employeelist
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/employeelist.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <h1>Employee list</h1>
+    <div>
+        <table border='1px'>
+            <tr>
+                <td>Name</td>
+                <td>Salary</td>
+                <td>Age</td>
+                
+            </tr>
+            <tr v-for ="item in list" v-bind:key="item.id">
+                <td>{{item.employee_name}}</td>
+                <td>{{item.employee_salary}}</td>
+                <td>{{item.employee_age}}</td>
+                
+                
+            </tr>
+
+        </table>
+    </div>
+
+    </div>
+</template>
+<script>
+import Vue from 'vue'
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+Vue.use(VueAxios,axios)
+export default {
+    name: "employeelist",
+    data(){
+        return {
+            list:undefined
+        }
+    },
+    mounted()
+    {
+        Vue.axios.get('http://dummy.restapiexample.com/api/v1/employees').then(resp=>{
+        this.list=resp.data.data
+        console.warn(resp.data.data)
+        })
+    }
+}
+</script>
